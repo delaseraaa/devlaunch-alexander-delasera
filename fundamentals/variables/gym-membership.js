72bfc373d240079cc239c9a7060f3b388c69bb00
Alexander De La Sera <alexanderdelasera23@outlook.com>
2025-04-09 15:48:56 -0600

    chore(variables): add gym-membership exercise

---
+++ fundamentals/variables/gym-membership.js
@@ -0,0 +1,29 @@
+/* El elite Fitness Center es un gimnasio boutique ubicado en Palo Alto, California 
+que ofrece membresias $150 al mes. 
+Opera de 6 AM a 10 PM , emplea a 10 entrenadores certificados.
+y cuenta con comodidades como sauna, piscina y clases grupales */
+
+const gymName = 'Elite Fitness Center'
+
+const gymLocation = 'Palo Alto, California'
+
+const gymPriceMonthly = '150 USD'
+
+const openingHour = '6:00 AM'
+const closingHour = '10:00 PM'
+
+const numberCoachesCertificate = '10 Entrenadores Certificados'
+
+const gymPlus = 'Sauna'
+const gymPlus2 = 'Piscina'
+const gymPlus3 = 'Clases Grupales'
+
+
+
+console.log(` Nombre Gimnasio: ${gymName})\n
+              Location: ${gymLocation})\n
+              Price: ${gymPriceMonthly}\n
+              Time: ${openingHour} ${closingHour}\n
+              Trainer: ${numberCoachesCertificate} certified trainers \n
+              Amenities: ${gymPlus},${gymPlus2},${gymPlus3}            
+`)
