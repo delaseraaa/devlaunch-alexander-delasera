2e2e5a4c24513b1a78dd099bd100ac882d40d551
Alexander De La Sera <alexanderdelasera23@outlook.com>
2025-04-09 19:08:27 -0600

    chore(variables): add supermarket exercise

---
+++ fundamentals/variables/supermarket.js
@@ -0,0 +1,35 @@
+/* Voy al super mercado Green Center para hacer mis compras habituales y planeo
+comprar manzanas y naranjas 
+Cuando estoy alli, veo que no hay platanos, ni uvas, asi que cambio un poco mi lista 
+Veo 3 cajeros estan trabajando, asi que me preparo para pagar.
+Mis cosas cuestan $100 en total 
+Entrego $150 en efectivo y recibo $50 de cambio */
+
+const nameMarket = 'Green Center'
+
+let platanos = 0
+
+let uvas = 0
+
+const favoriteFruit1 = 'manzanas'
+const favoriteFruit2 = 'naranjas'
+
+const numCajeros = '3 cajeros habilitados'
+
+const totalCompra = 100
+
+const currency = 'usd'
+
+const pago = 150
+
+const change = 50
+
+console.log(` Name Market: ${nameMarket}\n
+    Cant Uvas: ${uvas}\n
+    Purchased Fruit: ${favoriteFruit1}, ${favoriteFruit2}\n
+    Cant Platanos: ${platanos}\n
+    Total Cajas. ${numCajeros}\n
+    Total Pago: ${pago} ${currency}\n
+    Total Cambio: ${change} ${currency}\n
+       `)
+
