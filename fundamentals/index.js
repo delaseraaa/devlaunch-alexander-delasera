f7156d50c656bbb0e91d3e9bd366f741d9c124ac
Alexander De La Sera <alexanderdelasera23@outlook.com>
2025-04-11 00:46:42 -0600

    feat(operators): add folder structure

---
+++ fundamentals/index.js
@@ -0,0 +1,13 @@
+console.log(10 < 20) // true
+console.log(10 > 20) // false
+
+console.log(28 <= 28) // true
+console.log(28 >= 29) // false
+
+console.log(15 == 15) // true 
+console.log(6 != 9) // true
+
+console.log(0 != 0) // 
+
+5 === 5
+
