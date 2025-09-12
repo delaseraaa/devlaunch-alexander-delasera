7bc3fed72293e105a302e51f4d582692e8bafb4b
Alexander De La Sera <alexanderdelasera23@outlook.com>
2025-09-12 12:36:25 -0600

    Add App.js with basic React app code

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Agenda de Negocios</h1>
+      <p>Bienvenido a tu app de gestión de reuniones y contactos.</p>
+      {/* Aquí irán los componentes de eventos, contactos, etc. */}
+    </div>
+  );
+}
+
+export default App;
